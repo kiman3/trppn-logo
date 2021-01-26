da011fdec471356f46428bd8586136af1ef5b9b9
kiman3 <kim.andre.ottesen@gmail.com>
2021-01-26 22:07:56 +0100

    Relative audio path

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,23 +6,26 @@ const pauseBtn = document.getElementById('btnPause');
 // P5.js config
 let fft, input, inputSource;
 
+// FFT config
 const smoothing = 0;
-const binCount = 1024;
+const binCount = 128; // Must be a power of 2 between 16 and 1024 
 
-const size = 500;
+// Logo parameters
+const size = 1000;
 const center = size/2;
-const radiusOuter = size*0.4;
-const radiusInner = size*0.25;
+const radiusOuter = size*0.2;
+const radiusInner = size*0.125;
 const lines = 90;
 const rotation = 360/lines;
-const vertexWidthTop = size*0.008;
+const vertexWidthTop = radiusOuter*0.02;
 const vertexWidthBottom = vertexWidthTop/2;
 
+// Styles
 const colorBackground = 0;
 const colorFill = [0, 255, 225];
 
 // Input source
-inputSource = "http://localhost:8888/dev/trppn-logo/assets/audio/snippet.mp3";
+inputSource = "assets/audio/snippet.mp3";
 
 // Preload (if audio file)
 function preload(){
@@ -37,7 +40,7 @@ function setup(){
   vizCanvas.id('trppnSpectrum');
   vizCanvas.class('trppnSpectrum');
 
-  // Styles
+  // Set styles
   noStroke();
   background(colorBackground);
   fill(colorFill);
@@ -56,7 +59,7 @@ function draw(){
   clear();
 
   let level = amplitude.getLevel();
-  level = map(level, 0, 1, 1, 2)
+  level = map(level, 0, 1, 1, 3)
   let spectrum = fft.analyze();
 
   // Center + transform origin
